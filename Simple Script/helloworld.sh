1ad2465c1e3d1676f0a1224e7ec5f33fa399b6eb
Akhilhembram <sensi7ji@gmail.com>
2024-08-17 00:22:42 +0530

    Add files via upload

---
+++ Simple Script/helloworld.sh
@@ -0,0 +1,14 @@
+<<COMMENTS
+#!--> This is called shebang ,this is a special directive it indicate the starting of the code 
+
+--> For creating bash script you have to write the path of the bash folder generally it is /bin/bash
+
+--> Bash is a case sensitive language  
+
+COMMENTS
+
+
+#!/bin/bash
+
+echo "hello world"
+
