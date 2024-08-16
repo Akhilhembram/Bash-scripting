e953fc4220efc86d477115a8ced9e2d04d45524c
Akhilhembram <sensi7ji@gmail.com>
2024-08-17 00:20:57 +0530

    Add files via upload

---
+++ Simple Script/comments.sh
@@ -0,0 +1,41 @@
+<<COMMENTS 
+--> single line comments : #comment here   ( # is used to define a single line comment )
+
+--> 1st way multi line comments : (between angular bracket and comment words the comments are written )
+
+<<COMMENTS 
+
+your comment here 
+
+ COMMENTS
+
+--> 2nd way multi line comments : (between this colon and single qoutes the comments are written )
+
+ : ' 
+
+ your comment here 
+
+ ' 
+
+COMMENTS
+
+
+
+#!/bin/bash
+
+echo "single line comments : 
+
+#comment here  "
+
+echo "1st way multi line comments : 
+<<COMMENTS 
+
+your comment here 
+
+COMMENTS"
+
+echo "2nd way multi line comments : 
+
+ : ' 
+ your comment here 
+ ' "
